1e755126c0ed7ce18ea4582de30abecfbad17292
Syed Awase Khirni <awasekhirni@gmail.com>
2017-12-09 10:19:29 +0530

    Add files via upload

---
+++ mysql/onlinebank-mysql5-create.sql
@@ -0,0 +1,269 @@
+# ---------------------------------------------------------------------- #
+# Script generated with: DeZign for Databases v6.2.1                     #
+# Target DBMS:           MySQL 5                                         #
+# Project file:          onlinebank.dez                                  #
+# Project name:            online banking application                    #
+# Author:                 Syed Awase Khirni sak@territorialprescience    #
+# Script type:           Database creation script                        #
+# Created on:            2013-1-09 10:13                                #
+# ---------------------------------------------------------------------- #
+
+
+# ---------------------------------------------------------------------- #
+# Tables                                                                 #
+# ---------------------------------------------------------------------- #
+
+# ---------------------------------------------------------------------- #
+# Add table "t1accounts"                                                 #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `t1accounts` (
+    `t1f1accountid` VARCHAR(40) NOT NULL,
+    `t1f1accountisactive` BOOL,
+    `t2f2accounttypecode` VARCHAR(40),
+    `t3f3customerid` VARCHAR(40),
+    `t1f1accountname` VARCHAR(250),
+    `t1f1accountopendate` DATETIME,
+    `t1f1accountclosedate` DATETIME,
+    `t1f1accountdesc` VARCHAR(400),
+    `t1f1accountbalance` DOUBLE,
+    CONSTRAINT `PK_t1accounts` PRIMARY KEY (`t1f1accountid`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "t2refaccounttype"                                           #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `t2refaccounttype` (
+    `t2f2accounttypecode` VARCHAR(40) NOT NULL,
+    `t2f2accounttypedesc` VARCHAR(200),
+    `t2f2accounttyperemark` VARCHAR(100),
+    CONSTRAINT `PK_t2refaccounttype` PRIMARY KEY (`t2f2accounttypecode`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "t3customertype"                                             #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `t3customertype` (
+    `t3f3customercode` VARCHAR(40) NOT NULL,
+    `t3f3customerdesc` VARCHAR(200),
+    `t3f3customerremark` VARCHAR(100),
+    CONSTRAINT `PK_t3customertype` PRIMARY KEY (`t3f3customercode`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "t4customers"                                                #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `t4customers` (
+    `t3f3customerid` VARCHAR(40) NOT NULL,
+    `t3f3customername` VARCHAR(250),
+    `t3f3customeremail` VARCHAR(250),
+    `t3f3customerdesc` VARCHAR(40),
+    `t3f3customercode` VARCHAR(40),
+    `t3f3customerphone` VARCHAR(40),
+    `t3f3customerfrom` DATETIME,
+    `t3f3customerlastlogin` DATETIME,
+    `t3f3customerlastip` VARCHAR(100),
+    `t3f3pwdchangedate` DATETIME,
+    `t3f3transactionpassword` VARCHAR(40),
+    `t3f3currentbalance` DOUBLE,
+    CONSTRAINT `PK_t4customers` PRIMARY KEY (`t3f3customerid`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "t5refaddresstype"                                           #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `t5refaddresstype` (
+    `t5f5addresstypecode` VARCHAR(40) NOT NULL,
+    `t5f5addresstypedesc` VARCHAR(200),
+    `t5f5addresstyperemark` VARCHAR(100),
+    CONSTRAINT `PK_t5refaddresstype` PRIMARY KEY (`t5f5addresstypecode`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "t6addresses"                                                #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `t6addresses` (
+    `t6f6addressid` VARCHAR(40) NOT NULL,
+    `t6f6addressstreet` VARCHAR(250),
+    `t6f6city` VARCHAR(250),
+    `t6f6state` VARCHAR(250),
+    `t6f6country` VARCHAR(250),
+    `t6f6zip` VARCHAR(40),
+    `t6f6natcode` VARCHAR(40),
+    `t5f5addresstypecode` VARCHAR(40),
+    CONSTRAINT `PK_t6addresses` PRIMARY KEY (`t6f6addressid`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "t7customeraddresses"                                        #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `t7customeraddresses` (
+    `t7f7customeraddressid` VARCHAR(40) NOT NULL,
+    `t3f3customerid` VARCHAR(40),
+    `t6f6addressid` VARCHAR(40),
+    `t7f7datefrom` DATE,
+    `t7f7dateto` DATE,
+    CONSTRAINT `PK_t7customeraddresses` PRIMARY KEY (`t7f7customeraddressid`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "t8purchases"                                                #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `t8purchases` (
+    `t8f8purchaseid` VARCHAR(50) NOT NULL,
+    `t8f8purchaseamount` NUMERIC,
+    `t8f8taxpercent` NUMERIC,
+    `t8f8taxcode` VARCHAR(40),
+    `t8f8purchasedescription` VARCHAR(500),
+    `t8f8purchasetime` DATETIME,
+    `t3f3customerid` VARCHAR(40),
+    `t8f8quantity` NUMERIC,
+    CONSTRAINT `PK_t8purchases` PRIMARY KEY (`t8f8purchaseid`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "t9mechanttype"                                              #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `t9mechanttype` (
+    `t9f9merchanttypecode` VARCHAR(40) NOT NULL,
+    `t9f9merchanttypedesc` VARCHAR(200),
+    `t9f9merchanttyperemarks` VARCHAR(250),
+    CONSTRAINT `PK_t9mechanttype` PRIMARY KEY (`t9f9merchanttypecode`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "t10merchants"                                               #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `t10merchants` (
+    `t10f10merchantid` VARCHAR(40) NOT NULL,
+    `t10f10merchantname` VARCHAR(250),
+    `t10f10merchantdesc` VARCHAR(500),
+    `t9f9merchanttypecode` VARCHAR(40),
+    `t10f10merchantemail` VARCHAR(200),
+    `t10f10merchantphone` VARCHAR(40),
+    `t10f10merchantisactive` BOOL,
+    `t10f10merchanturl` VARCHAR(250),
+    `t10f10merchantaddress` VARCHAR(500),
+    `t10f10merchantcity` VARCHAR(250),
+    `t10f10merchantstate` VARCHAR(250),
+    `t10f10merchantcountry` VARCHAR(250),
+    `t10f10merchantzip` VARCHAR(40),
+    CONSTRAINT `PK_t10merchants` PRIMARY KEY (`t10f10merchantid`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "t11transactiontypes"                                        #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `t11transactiontypes` (
+    `t11f11transactiontypecode` VARCHAR(40) NOT NULL,
+    `t11f11transactiondesc` VARCHAR(250),
+    `t11f11transactionremarks` VARCHAR(250),
+    CONSTRAINT `PK_t11transactiontypes` PRIMARY KEY (`t11f11transactiontypecode`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "t12transactions"                                            #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `t12transactions` (
+    `t12f12transactionid` VARCHAR(50) NOT NULL,
+    `t12f12transactiondatetime` DATETIME,
+    `t12f12transactionstatus` VARCHAR(100),
+    `t11f11transactiontypecode` VARCHAR(40),
+    `t8f8purchaseid` VARCHAR(50),
+    `t1f1accountid` VARCHAR(40),
+    `t12f12transactionamount` DOUBLE,
+    CONSTRAINT `PK_t12transactions` PRIMARY KEY (`t12f12transactionid`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "t13balancehistory"                                          #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `t13balancehistory` (
+    `t13f13balanceatdate` DATETIME,
+    `t13f13balanceamount` DOUBLE,
+    `t1f1accountid` VARCHAR(40) NOT NULL,
+    CONSTRAINT `PK_t13balancehistory` PRIMARY KEY (`t1f1accountid`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "t14producttypes"                                            #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `t14producttypes` (
+    `t14f14producttypecode` VARCHAR(50) NOT NULL,
+    `t14f14producttypedesc` VARCHAR(200),
+    `t14f14producttyperemark` VARCHAR(400),
+    `t10f10merchantid` VARCHAR(40),
+    CONSTRAINT `PK_t14producttypes` PRIMARY KEY (`t14f14producttypecode`)
+);
+
+# ---------------------------------------------------------------------- #
+# Add table "t15products"                                                #
+# ---------------------------------------------------------------------- #
+
+CREATE TABLE `t15products` (
+    `t15productid` VARCHAR(50) NOT NULL,
+    `t15productdesc` VARCHAR(250),
+    `t15productimg` VARCHAR(250),
+    `t15productisactive` BOOL,
+    `t14f14producttypecode` VARCHAR(50),
+    CONSTRAINT `PK_t15products` PRIMARY KEY (`t15productid`)
+);
+
+# ---------------------------------------------------------------------- #
+# Foreign key constraints                                                #
+# ---------------------------------------------------------------------- #
+
+ALTER TABLE `t1accounts` ADD CONSTRAINT `t2refaccounttype_t1accounts` 
+    FOREIGN KEY (`t2f2accounttypecode`) REFERENCES `t2refaccounttype` (`t2f2accounttypecode`);
+
+ALTER TABLE `t1accounts` ADD CONSTRAINT `t4customers_t1accounts` 
+    FOREIGN KEY (`t3f3customerid`) REFERENCES `t4customers` (`t3f3customerid`);
+
+ALTER TABLE `t4customers` ADD CONSTRAINT `t3customertype_t4customers` 
+    FOREIGN KEY (`t3f3customercode`) REFERENCES `t3customertype` (`t3f3customercode`);
+
+ALTER TABLE `t6addresses` ADD CONSTRAINT `t5refaddresstype_t6addresses` 
+    FOREIGN KEY (`t5f5addresstypecode`) REFERENCES `t5refaddresstype` (`t5f5addresstypecode`);
+
+ALTER TABLE `t7customeraddresses` ADD CONSTRAINT `t4customers_t7customeraddresses` 
+    FOREIGN KEY (`t3f3customerid`) REFERENCES `t4customers` (`t3f3customerid`);
+
+ALTER TABLE `t7customeraddresses` ADD CONSTRAINT `t6addresses_t7customeraddresses` 
+    FOREIGN KEY (`t6f6addressid`) REFERENCES `t6addresses` (`t6f6addressid`);
+
+ALTER TABLE `t8purchases` ADD CONSTRAINT `t4customers_t8purchases` 
+    FOREIGN KEY (`t3f3customerid`) REFERENCES `t4customers` (`t3f3customerid`);
+
+ALTER TABLE `t10merchants` ADD CONSTRAINT `t9mechanttype_t10merchants` 
+    FOREIGN KEY (`t9f9merchanttypecode`) REFERENCES `t9mechanttype` (`t9f9merchanttypecode`);
+
+ALTER TABLE `t12transactions` ADD CONSTRAINT `t11transactiontypes_t12transactions` 
+    FOREIGN KEY (`t11f11transactiontypecode`) REFERENCES `t11transactiontypes` (`t11f11transactiontypecode`);
+
+ALTER TABLE `t12transactions` ADD CONSTRAINT `t8purchases_t12transactions` 
+    FOREIGN KEY (`t8f8purchaseid`) REFERENCES `t8purchases` (`t8f8purchaseid`);
+
+ALTER TABLE `t12transactions` ADD CONSTRAINT `t1accounts_t12transactions` 
+    FOREIGN KEY (`t1f1accountid`) REFERENCES `t1accounts` (`t1f1accountid`);
+
+ALTER TABLE `t13balancehistory` ADD CONSTRAINT `t1accounts_t13balancehistory` 
+    FOREIGN KEY (`t1f1accountid`) REFERENCES `t1accounts` (`t1f1accountid`);
+
+ALTER TABLE `t14producttypes` ADD CONSTRAINT `t10merchants_t14producttypes` 
+    FOREIGN KEY (`t10f10merchantid`) REFERENCES `t10merchants` (`t10f10merchantid`);
+
+ALTER TABLE `t15products` ADD CONSTRAINT `t14producttypes_t15products` 
+    FOREIGN KEY (`t14f14producttypecode`) REFERENCES `t14producttypes` (`t14f14producttypecode`);
---
+++ mysql/onlinebank-mysql5-drop.sql
@@ -0,0 +1,222 @@
+# ---------------------------------------------------------------------- #
+# Script generated with: DeZign for Databases v6.2.1                     #
+# Target DBMS:           MySQL 5                                         #
+# Project file:          onlinebank.dez                                  #
+# Project name:            online banking application                    #
+# Author:                 Syed Awase Khirni sak@territorialprescience    #
+# Script type:           Database creation script                        #
+# Created on:            2013-1-09 10:13                                #
+# ---------------------------------------------------------------------- #
+
+
+# ---------------------------------------------------------------------- #
+# Drop foreign key constraints                                           #
+# ---------------------------------------------------------------------- #
+
+ALTER TABLE `t1accounts` DROP FOREIGN KEY `t2refaccounttype_t1accounts`;
+
+ALTER TABLE `t1accounts` DROP FOREIGN KEY `t4customers_t1accounts`;
+
+ALTER TABLE `t4customers` DROP FOREIGN KEY `t3customertype_t4customers`;
+
+ALTER TABLE `t6addresses` DROP FOREIGN KEY `t5refaddresstype_t6addresses`;
+
+ALTER TABLE `t7customeraddresses` DROP FOREIGN KEY `t4customers_t7customeraddresses`;
+
+ALTER TABLE `t7customeraddresses` DROP FOREIGN KEY `t6addresses_t7customeraddresses`;
+
+ALTER TABLE `t8purchases` DROP FOREIGN KEY `t4customers_t8purchases`;
+
+ALTER TABLE `t10merchants` DROP FOREIGN KEY `t9mechanttype_t10merchants`;
+
+ALTER TABLE `t12transactions` DROP FOREIGN KEY `t11transactiontypes_t12transactions`;
+
+ALTER TABLE `t12transactions` DROP FOREIGN KEY `t8purchases_t12transactions`;
+
+ALTER TABLE `t12transactions` DROP FOREIGN KEY `t1accounts_t12transactions`;
+
+ALTER TABLE `t13balancehistory` DROP FOREIGN KEY `t1accounts_t13balancehistory`;
+
+ALTER TABLE `t14producttypes` DROP FOREIGN KEY `t10merchants_t14producttypes`;
+
+ALTER TABLE `t15products` DROP FOREIGN KEY `t14producttypes_t15products`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "t13balancehistory"                                         #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `t13balancehistory` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `t13balancehistory`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "t12transactions"                                           #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `t12transactions` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `t12transactions`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "t1accounts"                                                #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `t1accounts` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `t1accounts`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "t15products"                                               #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `t15products` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `t15products`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "t14producttypes"                                           #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `t14producttypes` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `t14producttypes`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "t11transactiontypes"                                       #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `t11transactiontypes` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `t11transactiontypes`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "t10merchants"                                              #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `t10merchants` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `t10merchants`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "t9mechanttype"                                             #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `t9mechanttype` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `t9mechanttype`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "t8purchases"                                               #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `t8purchases` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `t8purchases`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "t7customeraddresses"                                       #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `t7customeraddresses` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `t7customeraddresses`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "t6addresses"                                               #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `t6addresses` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `t6addresses`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "t5refaddresstype"                                          #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `t5refaddresstype` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `t5refaddresstype`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "t4customers"                                               #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `t4customers` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `t4customers`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "t3customertype"                                            #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `t3customertype` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `t3customertype`;
+
+# ---------------------------------------------------------------------- #
+# Drop table "t2refaccounttype"                                          #
+# ---------------------------------------------------------------------- #
+
+# Drop constraints #
+
+ALTER TABLE `t2refaccounttype` DROP PRIMARY KEY;
+
+# Drop table #
+
+DROP TABLE `t2refaccounttype`;
